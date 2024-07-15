25226ddb1575830f1bad00aaad733aa262c9d422
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2024-07-15 16:13:54 +0200

    Update

--- inotifywait.sh
+++ inotifywait.sh
@@ -1,3 +1,5 @@
 #!/bin/env bash
+# sometimes it catches filesystem updates from previous command so this sleep avoids that
+sleep 0.5
 inotifywait -e close_write -r . \
   --exclude 'dist-newstyle|.git|./cardano-node/example'
