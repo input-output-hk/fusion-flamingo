3f408ba0e8ebcaa88766bb8a7ae0290f5c433e4f
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2024-10-31 13:40:46 +0100

    Add colours to inotifywait, add semaphore and bump ghc to 9.10

--- inotifywait.sh
+++ inotifywait.sh
@@ -1,5 +1,19 @@
 #!/bin/env bash
+
+# colour stdout to make it more visible
+colour_stdout() {
+  if command -v lolcat > /dev/null 2>&1; then
+    while IFS= read -r line; do
+      echo "⚡ $line" | lolcat -a
+    done
+  else
+    while IFS= read -r line; do
+      echo -e "\e[35m⚡ $line\e[0m"
+    done
+  fi
+}
+
 # sometimes it catches filesystem updates from previous command so this sleep avoids that
 sleep 0.5
 inotifywait -e close_write -r . \
-  --exclude 'dist-newstyle|.git|./cardano-node/example'
+  --exclude 'dist-newstyle|.git|./cardano-node/example' | colour_stdout
